52f862e7c5627240c77d59d411127860f9ba837e
Jakub <jakub.jakubecky123@gmail.com>
2020-10-15 21:41:42 +0200

    c

--- main.cpp
+++ main.cpp
@@ -29,6 +29,10 @@ struct Rectangle{
         this->max->x = min->x + diffX;
         cout<<"";
     }
+    void moveNTimes(int n ){
+        int newY = min->y + stride->y;
+        moveToY(newY);
+    }
     Vector2I * getMinPos(){
         return min;
     }
@@ -98,13 +102,37 @@ int main() {
     args = split(line, ' ');
     Vector2I point(stoi(args[0]), stoi(args[1]));
     Rectangle * movesBox = new Rectangle(new Vector2I(0, 0), max, endPoint);
-    int shift = time / timeUsed;
-    movesBox->moveToY(shift);
+    int maxShift = time / timeUsed;
+    movesBox->moveNTimes(maxShift);
+
     int touches = 0;
     if(movesBox->getMinPos()->x <= point.x && movesBox->getMaxPos()->x >= point.x){
-        cout<<"here";
-        touches += 1;
+        Vector2I * checkingPoint = new Vector2I(movesBox->getMinPos()->x, movesBox->getMinPos()->y);
+        for(int i = 0; i < moves.size(); i ++){
+            int move = moves.at(i);
+            Vector2I * prevPoint = new Vector2I(checkingPoint->x, checkingPoint->y);
+            switch (pivot) {
+                case 0:
+                    checkingPoint->y += move;
+                    break;
+                case 1:
+                    checkingPoint->x += move;
+                    break;
+                case 2:
+                    checkingPoint->y -= move;
+                    break;
+                case 3:
+                    checkingPoint->x -= move;
+                    break;
+            }
+            if((prevPoint->y <= point.y && checkingPoint->y >= point.y) || (prevPoint->y >= point.y && checkingPoint->y <= point.y) ||
+                    (prevPoint->x <= point.x && checkingPoint->x >= point.x) || (prevPoint->x >= point.x && checkingPoint->x <= point.x)){
+                touches ++;
+            }
+            pivot += 1;
+            pivot %= 4;
+        }
     }
-    cout<<"OK";
+    cout<<touches;
     return 0;
 }
