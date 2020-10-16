47425140a83d2373ce31c9039147e0260f917e7b
Jakub <jakub.jakubecky123@gmail.com>
2020-10-16 10:59:39 +0200

    c

--- main.cpp
+++ main.cpp
@@ -2,32 +2,34 @@
 #include <iostream>
 
 #include <vector>
+#include <cmath>
+
 using namespace std;
-struct Vector2I{
-    int x;
-    int y;
-    Vector2I(int x, int y){
+struct Vector2f{
+    float x;
+    float y;
+    Vector2f(float x, float y){
         this->x = x;
         this->y = y;
     }
 };
 struct Rectangle{
-    Vector2I * min;
-    Vector2I * max;
-    Vector2I * stride;
-    Rectangle(Vector2I * min, Vector2I * max, Vector2I * stride){
+    Vector2f * min;
+    Vector2f * max;
+    Vector2f * stride;
+    Rectangle(Vector2f * min, Vector2f * max, Vector2f * stride){
         this->min = min;
         this->max = max;
         this->stride = stride;
     }
 
-    Vector2I * getMinPos(){
+    Vector2f * getMinPos(){
         return min;
     }
-    Vector2I * getSize(){
-        return new Vector2I(abs(min->x - max->x), abs(min->y - max->y));
+    Vector2f * getSize(){
+        return new Vector2f(abs(min->x - max->x), abs(min->y - max->y));
     }
-    Vector2I * getMaxPos(){
+    Vector2f * getMaxPos(){
         return max;
     }
 };
@@ -56,8 +58,8 @@ int main() {
     vector<int> moves;
     getline(cin, line);
     args = split(line, ' ');
-    Vector2I * max = new Vector2I(0,0);
-    Vector2I * endPoint = new Vector2I(0,0);
+    Vector2f * max = new Vector2f(0, 0);
+    Vector2f * endPoint = new Vector2f(0, 0);
     int timeUsed = 0;
     for(int i = 0; i < movesCount; i ++){
         int move = stoi(args[i]);
@@ -91,20 +93,28 @@ int main() {
     }
     getline(cin, line);
     args = split(line, ' ');
-    Vector2I point(stoi(args[0]), stoi(args[1]));
-    Rectangle * movesBox = new Rectangle(new Vector2I(0, 0), max, endPoint);
+    Vector2f point(stoi(args[0]), stoi(args[1]));
+    Rectangle * movesBox = new Rectangle(new Vector2f(0, 0), max, endPoint);
     int maxShift = time/timeUsed;//without the remaining small part
     int leftForMove = time % timeUsed;
     int touches = 0;
     if(movesBox->stride->y != 0) {
-        float a = (((float) movesBox->stride->x) / ((float) movesBox->stride->y));
-        int x = (point.y+a*point.x)/2*a;
-        int x1 = (point.y + a * point.x + movesBox->max->x)/2*a;
-        int x2 = (point.y + a * point.x - movesBox->max->y)/2*a;;
-        Vector2I p (x,  a*x);
-        Vector2I p1(x1, a*x1 - max->x);
-        Vector2I p2(x2, a*x2 + max->y);
+        float a = (movesBox->stride->x / movesBox->stride->y);
+        float x = (point.y + a*point.x)/(2*a);
+        float x1 = (point.y + a * point.x + movesBox->max->x*a)/(2*a);
+        float x2 = (point.y + a*point.x - movesBox->max->y)/(2*a);
+
+        Vector2f p (x, a * x);
+        Vector2f p1(x1, a * x1 - movesBox->max->x * a);
+        Vector2f p2(x2, a * x2 + movesBox->max->y);
         //get the distances
+
+        int distToPointSquared = pow(p.x - point.x, 2) + pow(p.y - point.y, 2);
+        if(((pow(p.x - p1.x, 2) + pow(p.y - p1.y, 2)) - distToPointSquared >= 0 ) ||
+            ((pow(p.x - p2.x, 2) + pow(p.y - p2.y, 2)) - distToPointSquared >= 0 )){
+            
+        }
+
     }
     return 0;
 }
