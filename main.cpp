e868986bc6c715cd6aabef45fc79383dbbf68471
Jakub <jakub.jakubecky123@gmail.com>
2020-10-17 01:26:55 +0200

    c

--- main.cpp
+++ main.cpp
@@ -19,12 +19,12 @@ struct Rectangle{
     Vector2f * size;
     Vector2f * firstStride;
     int endingPivot;
-    Rectangle(Vector2f * min, Vector2f * max,int endingPivot){
+    Rectangle(Vector2f * min, Vector2f * max, int firstMoveEndingPivot){
         this->min = min;
         this->max = max;
         this->size = new Vector2f(max->x - min->x, max->y - min->y);
        // this->firstStride = stride;
-        this->endingPivot = endingPivot;
+        this->endingPivot = firstMoveEndingPivot;
     }
     Vector2f * getNStride(int n){
         n %= 4;
@@ -99,19 +99,68 @@ int main() {
     getline(cin, line);
     vector<string> args = split(line, ' ');
     long int movesCount = stoll(args[0]);
-    long int time = stoll(args[1]);
-    int pivot = 0;
+    long int givenTime = stoll(args[1]);
 
-    vector<int> moves;
     getline(cin, line);
     args = split(line, ' ');
-    Vector2f * max = new Vector2f(0, 0);
-    Vector2f * endPoint = new Vector2f(0, 0);
-    int timePerCycle = 0;
+    /**
+     * counting the time per cycle here
+     */
+    int timePerCycle = 0;  //remember that the last cycle will take movesCount -1
+                                    // as the pivot wont be needed in the end
+    Vector2f * max;
+    int pivot = 0;
+    Vector2f * cycleEndPoint;//this in this case is the stride of the first cycle
+    {
+        int x = 0;
+        int y = 0;
+        int maxX = 0;
+        int maxY = 0;
+        for (int i = 0; i < movesCount; i++) {
+            int currMove = stoi(args[i]);
+            timePerCycle += currMove + 1;
+            switch (pivot) {
+                case 0:
+                    y += currMove;
+                    break;
+                case 1:
+                    x += currMove;
+                    break;
+                case 2:
+                    y -= currMove;
+                    break;
+                case 3:
+                    x -= currMove;
+                    break;
+            }
+            if (x > maxX)
+                maxX = x;
+            if (y > maxY)
+                maxY = y;
+            pivot += 1;
+            pivot %= 4;
+            if (i == (movesCount - 1)) {
+                cycleEndPoint = new Vector2f(x, y);
+                max = new Vector2f(maxX, maxY);
+            }
+        }
+    }
+
+
     int endingPivot = (movesCount) % 4;//pivot that is the start of the next cycle
     Rectangle * movesBox = new Rectangle(new Vector2f(0,0), max, endingPivot);
-    cout<<endingPivot<<"\n";
-    cout<<movesBox->getNCycleStartingPivot(5);
+
+    int cyclesToLoop = 1;   //starting with 1 so it wont take the first one.
+                            // The result of loop is the index a[n] that is the pivot
+    while(movesBox->getNCycleStartingPivot(cyclesToLoop) != 0 && cyclesToLoop++);
+
+    int timePerLoop = timePerCycle * cyclesToLoop;
+    Vector2f * cycleLoopStride = ?
+
+    //need the point after the loop to get and get the loop stride vector.
+    //From that the point where u can teleport to and if the givenTime is enough for such thing.
+    cout<<timePerLoop<<"\n";
+
 
     return 0;
 }
