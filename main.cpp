50967d09eb3e16d0b11a87c4be1e233300c3e821
Jakub <jakub.jakubecky123@gmail.com>
2020-10-16 18:37:38 +0200

    c

--- main.cpp
+++ main.cpp
@@ -16,13 +16,43 @@ struct Vector2f{
 struct Rectangle{
     Vector2f * min;
     Vector2f * max;
-    Vector2f * stride;
-    Rectangle(Vector2f * min, Vector2f * max, Vector2f * stride){
+    Vector2f * size;
+    Vector2f * firstStride;
+    int endingPivot;
+    Rectangle(Vector2f * min, Vector2f * max, Vector2f * stride, int endingPivot){
         this->min = min;
         this->max = max;
-        this->stride = stride;
+        this->size = new Vector2f(max->x - min->x, max->y - min->y);
+        this->firstStride = stride;
+        this->endingPivot = endingPivot;
     }
-
+    Vector2f * getNStride(int n){
+        n %= 4;
+        Vector2f * strideN;
+        switch (endingPivot) {
+            case 0:
+                if(n == 0) strideN = new Vector2f(firstStride->x,firstStride->y);
+                else if(n == 1) strideN = new Vector2f(firstStride->y,firstStride->x);
+                else if(n == 2) strideN = new Vector2f(-firstStride->x,-firstStride->y);
+                else if(n == 3) strideN = new Vector2f(-firstStride->y,-firstStride->x);
+                break;
+            case 1:
+                n %= 2;
+                if(n == 0) strideN = new Vector2f(firstStride->x,firstStride->y);
+                else if(n == 1) strideN = new Vector2f(-firstStride->x,-firstStride->y);
+                break;
+            case 2:
+                if(n == 0) strideN = new Vector2f(firstStride->x,firstStride->y);
+                else if(n == 1) strideN = new Vector2f(-firstStride->y,firstStride->x);
+                else if(n == 2) strideN = new Vector2f(-firstStride->x,-firstStride->y);
+                else if(n == 3) strideN = new Vector2f(firstStride->y,-firstStride->x);
+                break;
+            case 3:
+                strideN = firstStride;
+                break;
+        }
+        return strideN;
+    }
     Vector2f * getMinPos(){
         return min;
     }
@@ -32,18 +62,8 @@ struct Rectangle{
     Vector2f * getMaxPos(){
         return max;
     }
-    void changePos(Vector2f * shift){
-        float height = max->y - min->y;
-        float width = max->x - min->x;
-        min->x = shift->x;
-        min->y = shift->y;
-        max->y = min->y + height;
-        max->x = max->x + width;
-    }
     bool pointBelongs(Vector2f point){
-        if(min->x <= point.x && max->x >= point.x && min->y <= point.y && max->y >= point.y){
-            cout<<"im here";
-        }
+        return (min->x <= point.x && max->x >= point.x && min->y <= point.y && max->y >= point.y);
     }
 };
 vector<string> split(string str, char divider){
@@ -74,9 +94,11 @@ int main() {
     Vector2f * max = new Vector2f(0, 0);
     Vector2f * endPoint = new Vector2f(0, 0);
     int timePerCycle = 0;
+    int endingPivot;
     for(int i = 0; i < movesCount; i ++){
         int move = stoi(args[i]);
         if(timePerCycle + move + 1 > time){
+            endingPivot = pivot;
             break;
         }
         moves.push_back(move);
@@ -103,35 +125,14 @@ int main() {
         }
         pivot += 1;
         pivot %= 4;
+        if(i == movesCount -1)
+            endingPivot = pivot;
     }
     getline(cin, line);
     args = split(line, ' ');
     Vector2f point(stoi(args[0]), stoi(args[1]));
-    Rectangle * movesBox = new Rectangle(new Vector2f(0, 0), max, endPoint);
+    Vector2f * firstStride = new Vector2f(endPoint->x, endPoint->y);
+    Rectangle * movesBox = new Rectangle(new Vector2f(0,0), max, firstStride, endingPivot);
 
-    Vector2f * cycleVector = movesBox->stride;
-
-    int maxCycles = time / timePerCycle;
-    Vector2f * maxMove = new Vector2f(cycleVector->x * maxCycles, cycleVector->y * maxCycles);
-
-    int destOriginY = 0;
-    if(cycleVector->y != 0)
-        destOriginY = point.y - movesBox->getSize()->y;
-    int destOriginX = 0;
-    if(cycleVector->x != 0)
-        destOriginX = destOriginY/(cycleVector->y/cycleVector->x);
-
-    int touches = 0;
-    int timeLeft = time;
-    timeLeft -= 0;
-    Vector2f * neededMove = new Vector2f(destOriginX, destOriginY);
-    if(maxMove->x - neededMove->x >= 0 && maxMove->x*neededMove->x >= 0
-        && maxMove->y - neededMove->y >= 0 && maxMove->y*neededMove->y >= 0){
-        movesBox->changePos(neededMove);
-        if(movesBox->pointBelongs(point)){
-
-        }
-    }
-    cout<<touches;
     return 0;
 }
