c0a5f1766af851e148d05219523e63c094be60d9
Jakub <jakub.jakubecky123@gmail.com>
2020-10-18 00:44:32 +0200

    c

--- main.cpp
+++ main.cpp
@@ -15,15 +15,10 @@ struct Vector2f{
 };
 struct Rectangle{
     Vector2f * min;
-    Vector2f * max;
-    Vector2f * size;
     Vector2f * firstStride;
     int endingPivot;
-    Rectangle(Vector2f * min, Vector2f * max, int firstMoveEndingPivot){
+    Rectangle(Vector2f * min, int firstMoveEndingPivot){
         this->min = min;
-        this->max = max;
-        this->size = new Vector2f(max->x - min->x, max->y - min->y);
-       // this->firstStride = stride;
         this->endingPivot = firstMoveEndingPivot;
     }
     Vector2f * getNStride(int n){
@@ -61,23 +56,21 @@ struct Rectangle{
             case 3: return 3 - (n+3)%4;
         }
     }
+    static Vector2f * rotateStrideByPivot(int pivot, Vector2f * stride){
+        pivot %= 4;
+        switch (pivot) {
+            case 0: return stride;
+            case 1: return new Vector2f(stride->y, stride->x);
+            case 2: return new Vector2f(-stride->x, -stride->y);
+            case 3: return new Vector2f(-stride->y, -stride->x);
+        }
+    }
     Vector2f * getMinPos(){
         return min;
     }
-    Vector2f * getSize(){
-        return new Vector2f(abs(min->x - max->x), abs(min->y - max->y));
-    }
-    Vector2f * getMaxPos(){
-        return max;
-    }
-    bool pointBelongs(Vector2f point){
-        return (min->x <= point.x && max->x >= point.x && min->y <= point.y && max->y >= point.y);
-    }
     void move(Vector2f * v){
         this->min->x += v->x;
-        this->max->x += v->x;
         this->min->y += v->y;
-        this->max->y += v->y;
     }
 };
 vector<string> split(string str, char divider){
@@ -93,7 +86,9 @@ vector<string> split(string str, char divider){
     }
     return result;
 }
-
+bool isInt(float n){
+    return ((int)n == n);
+}
 int main() {
     string line;
     getline(cin, line);
@@ -108,9 +103,10 @@ int main() {
      */
     int timePerCycle = 0;  //remember that the last cycle will take movesCount -1
                                     // as the pivot wont be needed in the end
-    Vector2f * max;
+    //Vector2f * max;
     int pivot = 0;
-    Vector2f * cycleEndPoint;//this in this case is the stride of the first cycle
+    Vector2f * firstCycleEndPoint;//this in this case is the stride of the first cycle
+
     {
         int x = 0;
         int y = 0;
@@ -140,27 +136,49 @@ int main() {
             pivot += 1;
             pivot %= 4;
             if (i == (movesCount - 1)) {
-                cycleEndPoint = new Vector2f(x, y);
-                max = new Vector2f(maxX, maxY);
+                firstCycleEndPoint = new Vector2f(x, y);
+               // max = new Vector2f(maxX, maxY);
             }
         }
     }
-
+    getline(cin, line);
+    args = split(line, ' ');
+    Vector2f * subjectPoint = new Vector2f(stoi(args[0]), stoi(args[1]));
 
     int endingPivot = (movesCount) % 4;//pivot that is the start of the next cycle
-    Rectangle * movesBox = new Rectangle(new Vector2f(0,0), max, endingPivot);
+    Rectangle * movesBox = new Rectangle(new Vector2f(0,0), endingPivot);
 
     int cyclesToLoop = 1;   //starting with 1 so it wont take the first one.
                             // The result of loop is the index a[n] that is the pivot
     while(movesBox->getNCycleStartingPivot(cyclesToLoop) != 0 && cyclesToLoop++);
 
     int timePerLoop = timePerCycle * cyclesToLoop;
-    Vector2f * cycleLoopStride = ?
+    Vector2f * cycleLoopStride = new Vector2f(0,0);
+    for(int i = 0; i < cyclesToLoop; i ++){
+        int p = movesBox->getNCycleStartingPivot(i);
+        Vector2f * currVectorRotatedByPivot = movesBox->rotateStrideByPivot(p, firstCycleEndPoint);
+        cycleLoopStride = new Vector2f(cycleLoopStride->x + currVectorRotatedByPivot->x, cycleLoopStride->y + currVectorRotatedByPivot->y);
+    }
+    //now time to check where the maximum x and y that our path has
 
     //need the point after the loop to get and get the loop stride vector.
     //From that the point where u can teleport to and if the givenTime is enough for such thing.
     cout<<timePerLoop<<"\n";
 
+    int touches = 0;
+    if(cycleLoopStride->x != 0 && cycleLoopStride->y != 0){
+        float a = cycleLoopStride->y/cycleLoopStride->x;
+        Vector2f * destinedPointWithYEqualToPoint = new Vector2f(subjectPoint->y/a, subjectPoint->y - cycleLoopStride->y);
+    }
+    else if(cycleLoopStride->x == 0 && cycleLoopStride->y != 0){
+
+    }
+    else if(cycleLoopStride->y == 0 && cycleLoopStride->x != 0){
+
+    }
+    else{
+
+    }
 
     return 0;
 }
